ae806afae69a76e287d42675db0e2ddba97b46cc
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-08-14 21:26:54 +0900

    feat: 10 count씩 진동 기능 추가

--- app/src/main/java/dev/ehyeon/foregroundserviceexampleapplication/ForegroundService.java
+++ app/src/main/java/dev/ehyeon/foregroundserviceexampleapplication/ForegroundService.java
@@ -5,6 +5,8 @@ import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
@@ -40,6 +42,10 @@ public class ForegroundService extends Service {
                                 .build();
 
                 getSystemService(NotificationManager.class).notify(SERVICE_ID, notification);
+
+                if (count != 0 && count % 10 == 0) {
+                    ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(VibrationEffect.createOneShot(1000, VibrationEffect.DEFAULT_AMPLITUDE));
+                }
 
                 Log.i(TAG, "count = " + count++);
             }
